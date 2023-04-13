c0124fb79c8813c5a820c8efc7c6eec5916d9f2a
cyqa2001 <cyqa2001@gmail.com>
2023-04-13 23:14:04 +0800

    显示系统通知

--- src/main/java/cn/edu/scau/community/config/WebMvcConfig.java
+++ src/main/java/cn/edu/scau/community/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package cn.edu.scau.community.config;
 import cn.edu.scau.community.controller.interceptor.AlphaInterceptor;
 import cn.edu.scau.community.controller.interceptor.LoginRequiredInterceptor;
 import cn.edu.scau.community.controller.interceptor.LoginTicketInterceptor;
+import cn.edu.scau.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -20,6 +21,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
@@ -30,6 +34,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
     }
